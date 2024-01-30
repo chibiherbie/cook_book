baee817154f3ee91a3bd574296c4ddb8c8efe3e9
Roman <bekkerrdm@gmail.com>
2024-01-30 20:59:15 +0700

    add views

---
+++ src/project/cook_book/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('add_product_to_recipe/<int:recipe_id>/<int:product_id>/<int:weight>/', views.add_product_to_recipe,
+         name='add_product_to_recipe'),
+    path('show_recipes_without_product/<int:product_id>/', views.show_recipes_without_product,
+         name='show_recipes_without_product'),
+    path('cook_recipe/<int:recipe_id>/', views.cook_recipe, name='cook_recipe'),
+]
--- src/project/cook_book/views.py
+++ src/project/cook_book/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+from .models import Recipe, RecipeProduct, Product
 
-# Create your views here.
+
+def add_product_to_recipe(recipe_id, product_id, weight):
+    recipe = Recipe.objects.get(id=recipe_id)
+    product = Product.objects.get(id=product_id)
+    try:
+        recipe_product = RecipeProduct.objects.get(recipe=recipe, product=product)
+        recipe_product.weight = weight
+        recipe_product.save()
+    except RecipeProduct.DoesNotExist:
+        recipe.products.add(product, through_defaults={'weight': weight})
+
+
+def show_recipes_without_product(request, product_id=1):
+    recipes_without_product = Recipe.objects.exclude(recipeproduct__product_id=product_id).distinct()
+    recipes_with_low_quantity = Recipe.objects.filter(recipeproduct__product_id=product_id,
+                                                      recipeproduct__weight__lt=10).distinct()
+
+    template = loader.get_template('recipes_without_product.html')
+    context = {
+        'recipes_without_product': recipes_without_product,
+        'recipes_with_low_quantity': recipes_with_low_quantity,
+    }
+
+    return HttpResponse(template.render(context, request))
+
+
+def cook_recipe(recipe_id):
+    recipe = Recipe.objects.get(id=recipe_id)
+    recipe_products = recipe.products.all()
+    for recipe_product in recipe_products:
+        product = recipe_product.product
+        product.times_used += 1
+        product.save()
