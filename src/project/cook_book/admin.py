2c36d4288ba02270ca430a9cbcde28045defbda0
Roman <bekkerrdm@gmail.com>
2024-01-31 00:09:22 +0700

    update admin panel

--- src/project/cook_book/admin.py
+++ src/project/cook_book/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import Product, Recipe, RecipeProduct
 
-# Register your models here.
+
+class RecipeProductInline(admin.TabularInline):
+    model = RecipeProduct
+    extra = 1
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeProductInline]
+
+
 admin.site.register(Product)
-admin.site.register(Recipe)
+admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(RecipeProduct)
