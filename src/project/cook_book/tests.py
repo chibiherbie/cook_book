a077b4b98c70c9d5cff674a0f810c588eb0a5668
Roman <bekkerrdm@gmail.com>
2024-01-31 01:41:02 +0700

    add ModelTest

--- src/project/cook_book/tests.py
+++ src/project/cook_book/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Product, Recipe, RecipeProduct
+from django.urls import reverse
 
-# Create your tests here.
+
+class ModelTests(TestCase):
+    def setUp(self):
+        self.product = Product.objects.create(name='Помидоры')
+        self.recipe = Recipe.objects.create(name='Салат')
+
+    def test_product_str(self):
+        self.assertEqual(str(self.product), 'Помидоры')
+
+    def test_recipe_str(self):
+        self.assertEqual(str(self.recipe), 'Салат')
+
+    def test_add_product_to_recipe(self):
+        recipe_product = RecipeProduct.objects.create(recipe=self.recipe, product=self.product, weight=100)
+        self.assertEqual(recipe_product.recipe, self.recipe)
+        self.assertEqual(recipe_product.product, self.product)
+        self.assertEqual(recipe_product.weight, 100)
