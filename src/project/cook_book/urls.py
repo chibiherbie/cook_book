aa37b7d8ee572d5ec780ba73478ec614a13b027e
Roman <bekkerrdm@gmail.com>
2024-01-30 23:53:31 +0700

    edit urls

--- src/project/cook_book/urls.py
+++ src/project/cook_book/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from . import views
+from .views import add_product_to_recipe, cook_recipe, show_recipes_without_product
+
 
 urlpatterns = [
-    path('add_product_to_recipe/<int:recipe_id>/<int:product_id>/<int:weight>/', views.add_product_to_recipe,
+    path('add_product_to_recipe/<int:recipe_id>/<int:product_id>/<int:weight>/', add_product_to_recipe,
          name='add_product_to_recipe'),
-    path('show_recipes_without_product/<int:product_id>/', views.show_recipes_without_product,
+    path('show_recipes_without_product/<int:product_id>/', show_recipes_without_product,
          name='show_recipes_without_product'),
-    path('cook_recipe/<int:recipe_id>/', views.cook_recipe, name='cook_recipe'),
+    path('cook_recipe/<int:recipe_id>/', cook_recipe, name='cook_recipe'),
 ]
--- src/project/project/urls.py
+++ src/project/project/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('cook_book.urls')),
+    path('cookbook/', include('cook_book.urls')),
 ]
 
