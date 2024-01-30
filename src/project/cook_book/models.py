6f6d812082f41432b990cfecee4a3504f55e4a47
Roman <bekkerrdm@gmail.com>
2024-01-31 00:00:52 +0700

    fix models RecipeProduct - weight

--- src/project/cook_book/models.py
+++ src/project/cook_book/models.py
@@ -20,7 +20,7 @@ class Recipe(models.Model):
 class RecipeProduct(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    weight = models.IntegerField()
+    weight = models.IntegerField(default=0)
 
     def __str__(self):
         return f"{self.product.name} - {self.weight}гр"
