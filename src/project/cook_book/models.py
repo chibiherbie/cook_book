925525a78ec6be0fded0fa9028fb3e182319f0fc
Roman <bekkerrdm@gmail.com>
2024-01-30 18:12:41 +0700

    create model Recipe

--- src/project/cook_book/models.py
+++ src/project/cook_book/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100)
     count_used = models.IntegerField()
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    products = models.ManyToManyField(Product, through='RecipeProduct', related_name='recipes')
